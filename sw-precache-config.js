02daafed88ae5c91ca6b542036d23aeb59e198fc
Rodrigo Poi <rodrigo.poi@gmail.com>
2017-07-27 16:24:29 -0300

    ....

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,7 +5,8 @@ module.exports = {
     'dist/**.css',
     'dist/**.png',
     'dist/assets/images/*',
-    'dist/assets/icons/*'
+    'dist/assets/icons/*',
+    'dist/assets/fonts/*'
   ],
   root: 'dist',
   stripPrefix: 'dist/',
